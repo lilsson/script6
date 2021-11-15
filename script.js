326227f21ff9bc99c460cf32e96fd7214cbdc28f
lilsson <pirckhelavalile@gmail.com>
2021-11-15 15:18:10 +0300

    add error

--- script.js
+++ script.js
@@ -42,45 +42,87 @@
 // 1. xml http requist
 // 2. fetch
 
-function getUsers (){
+// function getUsers (){
 
-    function render (){
-        let response = this.responseText;
-        let responseData = JSON.parse(response);
+//     function render (){
+//         let response = this.responseText;
+//         let responseData = JSON.parse(response);
 
-        let ul = document.createElement ("ul");
+//         let ul = document.createElement ("ul");
         
         
-        responseData.data.forEach( item => {
-            let li = document.createElement('li');
-            li.textContent = item.last_name;
-            li.classList.add("show");
+//         responseData.data.forEach( item => {
+//             let li = document.createElement('li');
+//             li.textContent = item.last_name;
+//             li.classList.add("show");
             
             
-            ul.appendChild(li);
-        })
+//             ul.appendChild(li);
+//         })
 
 
 
-        document.getElementById("ips").appendChild(ul);
-        console.log(responseData);
-    }
+//         document.getElementById("ips").appendChild(ul);
+//         console.log(responseData);
+//     }
 
-    function errorRender() {
-        let p = document.createElement('p');
-        p.textContent = "server error";
+//     function errorRender() {
+//         let p = document.createElement('p');
+//         p.textContent = "server error";
+//         p.classList.add("error");
 
-        document.getElementById("ips").appendChild(p);
-    }
+//         document.getElementById("ips").appendChild(p);
+//     }
 
 
-    let requist = new XMLHttpRequest();
+//     let requist = new XMLHttpRequest();
  
-    requist.addEventListener("load", render);
-    requist.addEventListener("error", errorRender)
+//     requist.addEventListener("load", render);
+//     requist.addEventListener("error", errorRender)
 
-    requist.open("GET", "https://reqres.in/api/users?page=2");
-    requist.send();
-}
+//     requist.open("GET", "https://reqres.in/api/users?page=2");
+//     requist.send();
+// }
 
-getUsers();
+// getUsers();
+
+
+// fetch
+
+ fetch("https://reqres.in/api/users?page=2" , {
+           method: "GET"
+    })
+  .then(function(response) {
+      if (response.status !== 200) {
+          throw response.status;
+
+      } 
+      return response.json();
+  })
+  
+  .then(function(responseData) {
+      console.log(responseData);
+
+      let ul = document.createElement ("ul");
+      var fragment = document.createDocumentFragment();
+
+      responseData.data.forEach( item => {
+                    let li = document.createElement('li');
+                    li.textContent = item.last_name;
+                    li.classList.add("show");
+
+                    fragment.appendChild(li);
+
+      })
+         ul.appendChild(fragment);        
+        document.getElementById("ips").appendChild(ul);           
+  })
+  .catch(function(error) {
+    if(error == 404){
+        console.log("Page Not Found");
+    } else  if (error == 500){
+        console.log("Server Error");
+    } else{
+        console.log("error");
+    }
+  });
